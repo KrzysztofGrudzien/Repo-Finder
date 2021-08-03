a6dc18a6576306a4099227756a2dfe8bd5387fcc
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2021-08-03 23:32:21 +0200

    add propTypes for Users component

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,5 +1,6 @@
 import User from '../users/User';
 import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
 
 const Users = ({ users, isLoading }) => {
     if (!isLoading) {
@@ -13,6 +14,11 @@ const Users = ({ users, isLoading }) => {
     } else {
         return <Spinner />;
     }
+};
+
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    isLoading: PropTypes.bool.isRequired,
 };
 
 export default Users;
