a911dbbeb74ccd22205c2ae59be56e46b5278306
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2021-08-03 13:54:39 +0200

    install axios and fetch data from github API

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import React, { Component } from 'react';
 import NavBar from './components/layout/NavBar';
 import Users from './components/users/Users';
 import './App.css';
+import axios from 'axios';
+const baseUrl = 'https://api.github.com/users';
 
 class App extends Component {
+    state = {
+        users: [],
+        isLoading: false,
+    };
+
+    async componentDidMount() {
+        if (!this.state.isLoading) {
+            const response = await axios.get(baseUrl).then(response => response.data);
+            this.setState({ users: response, isLoading: false });
+        }
+    }
+
     render() {
         return (
             <div className='app'>
                 <NavBar />
-                <Users />
+                <Users users={this.state.users} isLoading={this.state.isLoading} />
             </div>
         );
     }
